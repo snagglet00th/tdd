a8f8a5432fd05dbf2e21d160eb3365ed39e22228
snaggletooth <snagglet00th@yandex.ru>
2019-07-08 22:24:28 +0500

    modify:add new checkinf to FT

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,9 @@
+import time
+
 from selenium import webdriver
 import unittest
+
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -24,16 +28,35 @@ class NewVisitorTest(unittest.TestCase):
         # неотложных дел
 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ей сразу же предлагается ввести элемент списка
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
         # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби –
-        # вязание рыболовных мушек)
+        input_box.set_keys(Keys.ENTER)
+        time.sleep(1)
+
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+
+        self.fail('Закончить тест!')
+
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес – об этом
