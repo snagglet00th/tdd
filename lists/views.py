c12803680675a422d2f2460c020183c6743b6e93
snaggletooth <snagglet00th@yandex.ru>
2019-07-12 14:33:45 +0500

    fix:add redirect302 support after POST -> views.home_page

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from lists.models import Item
 
@@ -8,10 +8,8 @@ def home_page(request):
     if request.method == 'POST':
         new_item = request.POST['item_text']
         Item.objects.create(text=new_item)
-    else:
-        new_item = ''
+        return redirect('/')
 
-    return render(request,
-                  'lists/home.html', {'new_item_text': new_item})
+    return render(request, 'lists/home.html')
 
 # POST.get('key', val) - если запрос не POST['key']=val, то тогда вернуть GET
