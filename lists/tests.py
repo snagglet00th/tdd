c3956fa83f3817b13468fb0db5c5b664d3a63f90
snaggletooth <snagglet00th@yandex.ru>
2019-07-12 13:58:00 +0500

    modify:adding check into test for create db item

--- lists/tests.py
+++ lists/tests.py
@@ -8,6 +8,9 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
+    def setUp(self):
+        self.FIRST_LIST_ITEM = 'A new list item'
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -18,8 +21,13 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'lists/home.html')
 
     def test_can_save_a_post_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
+        response = self.client.post('/', data={'item_text': self.FIRST_LIST_ITEM})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()  # or Item.objects.all()[0]
+        self.assertEqual(new_item.text, self.FIRST_LIST_ITEM)
+
+        self.assertIn(self.FIRST_LIST_ITEM, response.content.decode())
         self.assertTemplateUsed(response, 'lists/home.html')
 
 
--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from lists.models import Item
+
 
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
     return render(request,
-                  'lists/home.html',
-                  {'new_item_text': request.POST.get('item_text', '')})
+                  'lists/home.html', {'new_item_text': item.text
+                                      })
 
 # POST.get('key', val) - если запрос не POST['key']=val, то тогда вернуть GET
-
-
-
